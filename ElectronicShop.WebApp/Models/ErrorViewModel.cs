5143b3acd903ed7b54f57b423ddc0757209914dd
DESKTOP-MUM11D5\TrongHieu <hieutrong0123@gmail.com>
2021-03-02 22:50:34 +0700

    Initial commit

---
+++ ElectronicShop.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElectronicShop.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
