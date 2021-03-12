649baee955f34a14076f6ccf08f3e13a0530f96e
DESKTOP-MUM11D5\TrongHieu <hieutrong0123@gmail.com>
2021-03-12 20:34:39 +0700

    commit webapi structure and swagger

---
+++ ElectronicShop.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectronicShop.Application.Products.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronicShop.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var products = await _productService.GetAllProduct();
+            return Ok(products);
+        }
+    }
+}
